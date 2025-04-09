a048213b9fb10e03053a3ee74b740ec193cbe883
MarceloMartinez7 <mr5405750@gmail.com>
2025-04-08 23:08:28 -0600

    Avances Diseno

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,14 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import './Inicio.css';
 
 const Inicio = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
   return (
-    <div>
-      <br />
-      <br />
-      <h1>Bienvenido a Scholaria</h1>
-      <p>Tu espacio para gestionar grados, estudiantes, docentes y mucho más.</p>
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h2>Bienvenido</h2>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}>
+        <div className="boton-imagen" onClick={() => handleNavigate("/estudiantes")}>
+          <img src="src/assets/imagenes/EstudiantesBoton.png" alt="Estudiantes" />
+        </div>
+
+        <div className="boton-imagen" onClick={() => handleNavigate("/Docentes")}>
+          <img src="src/assets/imagenes/DocentesBoton.png" alt="Docentes" />
+        </div>
+
+        <div className="boton-imagen" onClick={() => handleNavigate("/Asignatura")}>
+          <img src="src/assets/imagenes/EventosBoton.png" alt="Asignaturas" />
+        </div>
+
+        <div className="boton-imagen" onClick={() => handleNavigate("/Asistencia")}>
+          <img src="src/assets/imagenes/AsistenciaBoton.png" alt="Asistencia" />
+        </div>
+
+        <div className="boton-imagen" onClick={() => handleNavigate("/CatalogoCalificaciones")}>
+          <img src="src/assets/imagenes/CalificacionesBoton.png" alt="Catálogo de Calificaciones" />
+        </div>
+
+        <div className="boton-imagen" onClick={() => handleNavigate("/Calendario")}>
+          <img src="src/assets/imagenes/EventosBoton.png" alt="Calendario" />
+        </div>
+
+        <div className="boton-imagen" onClick={() => handleNavigate("/Calificaciones")}>
+          <img src="src/assets/imagenes/EventosBoton.png" alt="Calificaciones" />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Inicio;
-
