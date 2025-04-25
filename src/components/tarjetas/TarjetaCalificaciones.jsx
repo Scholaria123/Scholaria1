397cee4f29fb8d36ccfceced4753b47be6b79d3c
MarceloMartinez7 <mr5405750@gmail.com>
2025-04-24 22:36:43 -0600

    Correccion en la ventana de busqueda de estudiantes

--- src/components/tarjetas/TarjetaCalificaciones.jsx
+++ src/components/tarjetas/TarjetaCalificaciones.jsx
@@ -5,15 +5,25 @@ const TarjetaCalificaciones = ({ estudiante }) => {
     <Col lg={3} md={4} sm={12} className="mb-4">
       <Card>
         {estudiante.imagen && (
-          <Card.Img variant="top" src={estudiante.imagen} alt={estudiante.nombre} />
+          <Card.Img
+            variant="top"
+            src={estudiante.imagen}
+            alt={estudiante.nombre}
+            style={{
+              width: "100%",  
+              height: "200px",  
+              objectFit: "cover",  
+              borderTopLeftRadius: "10px",  
+              borderTopRightRadius: "10px",
+            }}
+          />
         )}
         <Card.Body>
           <Card.Title>{estudiante.nombre}</Card.Title>
           <Card.Text>
-           Grado: {estudiante.grado} <br />
-           Grupo: {estudiante.grupo} <br />
+            Grado: {estudiante.grado} <br />
+            Grupo: {estudiante.grupo} <br />
           </Card.Text>
-
         </Card.Body>
       </Card>
     </Col>
